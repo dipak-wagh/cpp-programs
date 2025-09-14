57db27df806c10650fd2e7bb8df3d0776dc517c4
dipak-wagh <dipakwagh0502@gmail.com>
2025-09-14 16:44:09 +0530

    Added today's pattern programs

---
+++ pattern10.cpp
@@ -0,0 +1,27 @@
+/*
+1
+21
+321
+4321
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cin>>num;
+    int i=1;
+    while(i<=num){
+        int j=1;
+        while(j<=i){
+            cout<<i-j+1;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+
+
+
+return 0;
+}
---
+++ pattern13.cpp
@@ -0,0 +1,28 @@
+/*
+A B C
+D E F
+G H I
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+        char ch='A';
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<ch<<" ";
+            ch++;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+
+
+
+return 0;
+}
