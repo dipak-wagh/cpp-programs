84a613b46228fd76708a4397dfe368055f8b46f7
Dipak Wagh <dipakwagh0502@gmail.com>
2026-04-11 00:07:29 +0530

    Array programs

---
+++ Arrays/Duplicated_Elements.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 2, 3,4, 1};
+
+    for(int i = 0; i < 7; i++) {
+            if(arr[i] == arr[j]) {
+                cout << arr[i] << " ";
+            }
+        }
+    }
+    return 0;
+}
